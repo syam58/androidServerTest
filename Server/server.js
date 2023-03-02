d2181fead4a70358cf4f3cf5d5576b234da24d5d
syam v <64647318+syam58@users.noreply.github.com>
2023-03-02 12:15:33 +0530

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: ["http://192.168.152.250:5173","http://localhost:5173"],
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	  console.log("connection established");
+	
+	socket.on("join-room", (data) => {
+	  console.log(data);
+	  
+		socket.join(data.room);
+	})
+
+	socket.on("send-message", (data) => {
+		console.log(data);
+		
+		socket.to(data.room).emit("receive-message", {name:data.name,message:data.message})
+	})
+	socket.on("upload", (data) => {
+		socket.to(data.room).emit("receive-upload", {name:data.name,message:data.message,type:data.type})
+	})
+	
+
+	socket.on("disconnect", () => {
+		//socket.broadcast.emit("callEnded")
+	})
+	
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
